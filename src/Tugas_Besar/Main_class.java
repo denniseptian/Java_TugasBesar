1308eb944c00ee94c73bbf341042e2f338a92f8d
denniseptian <dennix.apk@gmail.com>
2015-05-04 19:30:21 +0700

    new

---
+++ src/Tugas_Besar/Main_class.java
@@ -0,0 +1,36 @@
+package Tugas_Besar;
+
+public class Main_class {
+	 
+	public static void main(String [] args){
+		Input InputObject = new Input();
+		InputObject.master_input_1();
+		InputObject.tampil();
+		InputObject.luas_ruangan();
+		InputObject.Penkondisian_luas();
+		InputObject.master_input_3();
+		InputObject.rasio_luas();
+		InputObject.Pengkondisian_rasio();
+		InputObject.master_input_2();
+		InputObject.analisis_jumlah_cendela_dan_pintu();
+		InputObject.master_input_4();
+		InputObject.analisis_kelistrikan();
+		InputObject.master_input_5();
+		InputObject.pengkondisian_kabel_lcd();
+		InputObject.master_input_6();
+		InputObject.penkondisian_lampu();
+		InputObject.master_input_7();
+		InputObject.pengkondisian_kipas_angin();
+		InputObject.master_input_8();
+		InputObject.pendkondisian_ac();
+		InputObject.InputSSID();
+		InputObject.pengkondisian_SSID();
+		InputObject.input_CCTV();
+		InputObject.pengkondisian_CCTV();
+		InputObject.pengkondisian_kebersihan();
+		InputObject.input_sirkulasi_udara();
+		InputObject.penkondisian_sirkulasi_udara();
+		InputObject.input_pencahayaan();
+		InputObject.penkondisian_cahaya();
+	}
+}
