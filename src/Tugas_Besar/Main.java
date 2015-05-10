8f949f3e90dc9944a9b4b76b6d7e0a485a897d2c
Dennis <denniseptianhermawan@gmail.com>
2015-05-10 10:05:09 +0700

    new

--- src/Tugas_Besar/Main.java
+++ src/Tugas_Besar/Main.java
@@ -1,10 +1,8 @@
 package Tugas_Besar;
 
-public class Main_class {
-	 
+public class Main {
 	public static void main(String [] args){
 		Input InputObject = new Input();
 		InputObject.Master_input();
-		
 	}
 }
